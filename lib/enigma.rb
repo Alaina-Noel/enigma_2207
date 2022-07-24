6eb498daafce8bd372aef27f7dca006f9c210d57
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-24 13:58:21 -0600

    Add a partially working encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,6 +4,11 @@ class Enigma
   end
 
   def encrypt(message, key, date)
+    encrypted_hash = Hash.new(0)
+    encrypted_hash[:encryption] = "Encrypted Message PlaceHolder"
+    encrypted_hash[:key] = key
+    encrypted_hash[:date] = date
+    encrypted_hash
   end
 
 end
