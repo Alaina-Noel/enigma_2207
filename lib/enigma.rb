a884234ccfe0582dc519bb3ce879ede5a92102d1
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-01 18:15:20 -0600

    Add an encrypt message method that will take first steps in mutating the message

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,26 @@
 class Enigma
+  attr_reader :message
 
   def initialize
+    @message = nil
+  end
+
+  def encrypt_message(input)
+    input.upcase
+    #where should this method live?
   end
 
   def encrypt(message, key, date)
+    #intake the 3 parameters, pass them to where they need to go
+    #return them back here and spit them out into a hash
+    @message = message
+    encrypted_message = encrypt_message(@message) #haven't made this method yet
     encrypted_hash = Hash.new(0)
-    encrypted_hash[:encryption] = "Encrypted Message PlaceHolder"
+    encrypted_hash[:encryption] = encrypted_message
     encrypted_hash[:key] = key
     encrypted_hash[:date] = date
     encrypted_hash
   end
+
 
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -17,6 +17,48 @@ RSpec.describe do
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq(hash)
   end
 
+  xit 'can decrypt a message' do
+    enigma = Enigma.new
+    hash =   {
+            decryption: "hello world",
+            key: "02715",
+            date: "040895"
+            }
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(hash)
+  end
+
+  xit 'can encrypt a message that includes characters not in character set' do
+    enigma = Enigma.new
+    hash = {
+          encryption: "keder ohulw!$",
+          key: "02715",
+          date: "040895"
+          }
+    expect(enigma.encrypt("hello world!$", "02715", "040895")).to eq(hash)
+  end
+
+  xit 'can encrypt a message that includes capital letters' do
+    enigma = Enigma.new
+    hash = {
+          encryption: "keder oh$$w!",
+          key: "02715",
+          date: "040895"
+          }
+    expect(enigma.encrypt("HELLO PI$$A!", "02715", "040895")).to eq(hash)
+  end
+
+  xit 'can pad the randomly gemerated numbers with 00 on the left side' do
+    enigma = Enigma.new
+    hash = {
+          encryption: "keder ohulw",
+          key: "02715",
+          date: "040895"
+          }
+    expect(enigma.encrypt("hello world", "2715", "040895")).to eq(hash)
+  end
+
+
+
 
 
 end
