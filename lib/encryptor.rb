c97a51f52bd48a6f4de1066776937eb141be1b37
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-05 17:04:59 -0600

    Pass decrypt test

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -26,7 +26,26 @@ class Encryptor
     result
   end
 
-
+  def apply_unshift(message, shifter)
+    result = ""
+    message.each_char.with_index do |char, index|
+      if !@char_set.include?(char)
+        result << char
+      else
+        start = @char_set.find_index(char)
+        if index % 4 == 0
+          result << @char_set.rotate(start - shifter[:A]).first
+        elsif index % 4 == 1
+          result << @char_set.rotate(start - shifter[:B]).first
+        elsif index % 4 == 2
+          result << @char_set.rotate(start - shifter[:C]).first
+        elsif index % 4 == 3
+          result << @char_set.rotate(start - shifter[:D]).first
+        end
+      end
+    end
+    result
+  end
 
 
 end
