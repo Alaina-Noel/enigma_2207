b6df677a9035581f2704493ae99be3340f659880
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-04 19:55:18 -0600

    Planning out next moves

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,2 +1,17 @@
 class Encryptor
+
+  def initialize
+    #I want to initialize it with a shift_generator objext
+  end
+
+  def apply_shift
+#how do I get these classes to recieve each others information without
+#doing something like enigma.encryptor.apply_shift
+  end
+
+  #maybe I need to condense things more and say that enigma can contain the method
+  #apply shift and have instance variables of shift_generator and those isntance
+  #variables can have methods?
+
+
 end
