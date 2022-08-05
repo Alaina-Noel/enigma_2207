b256ddf5016982c85b33ef3526580fb20374fca0
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-05 12:16:15 -0600

    pass tests for encryptor when characters are not included in char set

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -8,16 +8,21 @@ class Encryptor
   def apply_shift(message, shifter)
     result = ""
     message.each_char.with_index do |char, index|
-      start = @char_set.find_index(char)
-      result << char if !@char_set.include?(char)
-      if index % 4 == 0
-        result << @char_set.rotate(shifter[:A] + start).first
-      elsif index % 4 == 1
-        result << @char_set.rotate(shifter[:B] + start).first
-      elsif index % 4 == 2
-        result << @char_set.rotate(shifter[:C] + start).first
-      elsif index % 4 == 3
-        result << @char_set.rotate(shifter[:D] + start).first
+      # require "pry"
+      # binding.pry
+      if !@char_set.include?(char)
+        result << char
+      else
+        start = @char_set.find_index(char)
+        if index % 4 == 0
+          result << @char_set.rotate(shifter[:A] + start).first
+        elsif index % 4 == 1
+          result << @char_set.rotate(shifter[:B] + start).first
+        elsif index % 4 == 2
+          result << @char_set.rotate(shifter[:C] + start).first
+        elsif index % 4 == 3
+          result << @char_set.rotate(shifter[:D] + start).first
+        end
       end
     end
     result
--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe do
     expect(encryptor.apply_shift("hello world", shifter)).to eq("keder ohulw")
   end
 
-  xit 'can apply a shift to a message with extra characters' do
+  it 'can apply a shift to a message with extra characters' do
     encryptor = Encryptor.new
     shifter = {A: 3, B: 27, C: 73, D: 20}
 
