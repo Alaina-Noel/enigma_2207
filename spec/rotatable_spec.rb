58c95569128314b22a8f2de67e09badaa3933482
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-06 11:24:06 -0600

    Create rotatable tests for module

---
+++ spec/rotatable_spec.rb
@@ -0,0 +1,21 @@
+require './lib/encryptor'
+require './lib/shift_generator'
+require './lib/rotatable'
+
+RSpec.describe do
+  it 'can rotate a single character' do
+    encryptor = Encryptor.new
+    shift_generator = ShiftGenerator.new
+    shifter = shift_generator.create_shifter("02715", "040895")
+
+    expect(encryptor.rotate_char("h", 4, shifter)).to eq("k")
+  end
+
+  it 'can un-rotate a single character' do
+    encryptor = Encryptor.new
+    shift_generator = ShiftGenerator.new
+    shifter = shift_generator.create_shifter("02715", "040895")
+
+    expect(encryptor.unrotate_char("k", 4, shifter)).to eq("h")
+  end
+end
