af9497c50848e11033782c4aab94934c0313933c
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-06 10:55:09 -0600

    Take awway rotation applier method because it does not simplify things

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -15,14 +15,6 @@ RSpec.describe do
     expect(encryptor.char_set).to eq(char_set_expected)
   end
 
-  it 'can rotate a character according to a shift letter' do
-    encryptor = Encryptor.new
-    shift_generator = ShiftGenerator.new
-    shifter = shift_generator.create_shifter("02715", "040895")
-
-    expect(encryptor.apply_rotation("h", :A, shifter)).to eq("k")
-  end
-
   it 'can apply a shift to a message' do
     encryptor = Encryptor.new
     shift_generator = ShiftGenerator.new
