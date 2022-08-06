65303322d37d53e0ba99914f44892ea49dfd2bf3
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-06 10:29:43 -0600

    Completed CLI

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,13 +3,11 @@ require './lib/shift_generator'
 require './lib/encryptor'
 
 file1 = File.open("#{ARGV[0]}", "r")
-# require "pry"
-# binding.pry
 message_from_file = file1.read.strip
 enigma = Enigma.new
 encrypted = enigma.encrypt(message_from_file)
 file2 = File.open("#{ARGV[1]}", "w")
-file2.write(encrypted)
+file2.write(encrypted[:encryption])
 file2.close
 puts "Created '#{ARGV[1]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
 #need to take care of multiline messages
